27e16df7853f12465eaa3dd729dad4e77a89a44f
MadhuSudhanan11 <97581180+MadhuSudhanan11@users.noreply.github.com>
2022-01-19 18:35:00 +0530

    Add files via upload

---
+++ 07-01-2022/divide.java
@@ -0,0 +1,9 @@
+class divide
+{
+public static void main(String args[])  
+{    
+int x=20,y=2,sum;
+sum = x/y;
+System.out.println(sum);
+}
+}
---
+++ 07-01-2022/sum.java
@@ -0,0 +1,9 @@
+class sum
+{
+public static void main(String args[])  
+{    
+int x=20,y=15,sum;
+sum = x+y;
+System.out.println(sum);
+}
+}

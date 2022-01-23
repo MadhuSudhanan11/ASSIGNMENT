aecf34371ccd1efb4a5b669e0518b1128b624093
MadhuSudhanan11 <97581180+MadhuSudhanan11@users.noreply.github.com>
2022-01-23 20:57:40 +0530

    Add files via upload

---
+++ 20-01-2022/Student.java
@@ -0,0 +1,13 @@
+class Student
+{
+	String name;
+	int roll_no;
+	public static void main(String args[])
+    {
+	Student obj =new Student();
+	obj.name="Madhu";
+	obj.roll_no=37;
+	System.out.println("Student name : "+obj.name);
+	System.out.println("Roll no : "+obj.roll_no);
+    }
+}
---
+++ 20-01-2022/Triangle.java
@@ -0,0 +1,15 @@
+class Triangle
+{
+	public Triangle()
+	{
+		int a=5;
+		int b=7;
+		int c=9;
+		System.out.println((a*b)/2);
+		System.out.println(a+b+c);
+	}
+	public static void main (String args[])
+	{
+		Triangle obj=new Triangle();
+	}
+}
